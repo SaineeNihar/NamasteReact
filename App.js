45a2960950db19cc9fe994cf498e66c693b3d9cc
SaineeNihar <nihar.sainee@gmail.com>
2024-10-04 20:50:20 +0530

    jsx

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement(
   "h1",
-  { id: "header" },
-  "Hello From React!!!!!"
+  { id: "heading" },
+  "Hello From Namaste React"
 );
+
+const jsxHeading = <h1 id="heading">Hello from JSX heading👹</h1>;
+console.log(heading);
+console.log(jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
